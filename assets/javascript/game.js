42c065f7d4f5456076be1289a6c4f250c66878d5
redricasa <fredeb26@gmail.com>
2019-08-15 14:10:52 -0700

    added a ./before assets to link to JS file. added a function to generate random number for crystals and another for goal amount. added HW instructions and psudocode in README file

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+//random number generator function for crystals - min of 1 and max of 12 add 1 to start the count from 1 instead of the inclusive number 0
+var crystalGenerator = function (){
+    return console.log(Math.floor(Math.random()* 13) + 1);
+}
+    
+//Crystal amount needed random number generator function 
+var goalGenerator = function (max, min){
+    var max = 120;
+    var min = 19;
+    return console.log( (Math.floor(Math.random() * (max - min + 1))+ min) );
+};
+
+
+
+
+
