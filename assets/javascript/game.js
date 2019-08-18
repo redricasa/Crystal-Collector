c996f966e9887d0565c1a30b4103922ccb5ff380
redricasa <fredeb26@gmail.com>
2019-08-17 21:56:01 -0700

    added event handler function, modified HTML

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,68 @@
 //random number generator function for crystals - min of 1 and max of 12 add 1 to start the count from 1 instead of the inclusive number 0
 $(document).ready(function(){
     var crystalGenerator = function (){
-        return console.log(Math.floor(Math.random()* 13) + 1);
+        return (Math.floor(Math.random()* 13) + 1);
     };    
     //Crystal amount needed random number generator function 
     var goalGenerator = function (max, min){
-        var max = 120;
-        var min = 19;
         return (Math.floor(Math.random() * (max - min + 1))+ min);
     };
-    // var accumulated = function(){};//0000
-    console.log (goalGenerator() + " goal");
-    var win= win++;
-    var loss= loss++;
+    const max = 120;
+    const min = 19;
+    var goalval = goalGenerator(max, min);
+    $("#random-goal").text(goalval);
+    console.log (goalval + " goal");
+    var win= 0;
+    var loss= 0;
     var accumulated=0;
     // var blue = function blue (){};
-    $("#gold").click(crystalGenerator);
-    console.log(crystalGenerator()+" gold");
-    $("#green").click(crystalGenerator);
-    console.log(crystalGenerator()+" green");
-    $("#purple").click(crystalGenerator);
-    console.log(crystalGenerator()+" purple");
-    $("#blue").click(crystalGenerator);
-    console.log(crystalGenerator()+" blue");
-
-    if (accumulated===goalGenerator()){
-        $("#win").html(win++);
-    };
-    var status=function(){
-        if(){
-            $("#status").html("<p>You won!!</p>");
-        };
+    var crystalEventHandler = function (eventObject){
+        //extract the value out of crystal
+        var val = parseInt($(this).attr('samri'), 10);
+        //add that value to accumulated
+        accumulated= accumulated+val;
+        console.log("the extracted value is "+ val );
+        console.log("the accumulated amount is " + accumulated);
+        $("#accumulated").text(accumulated);
+        if (accumulated===goalval){
+            $("#win").text(++win);
+        }
+        else if (accumulated > goalval){
+            $("#loss").text(++loss);
+        }
         else{
-            $("#status").html("<p>You lost!</p>");
-        };        
+            console.log("keep playing")
+        }
     };
+
+    $(".crystal").click(crystalEventHandler);
+
+    var goldval = crystalGenerator();
+    $("#gold").attr('samri', goldval);//to link the crystalGnerator # to #gold
+    console.log(goldval+" gold");
+
+    var greenval = crystalGenerator();
+    $("#green").attr('samri', greenval);
+    console.log(greenval+" green");
+
+    var purpleval= crystalGenerator();
+    $("#purple").attr('samri', purpleval);
+    console.log(purpleval+" purple");
+
+    var blueval = crystalGenerator();
+    $("#blue").attr('samri', blueval);
+    console.log(blueval+" blue");
+
+    
+    
+    // var status=function(){
+    //     if(){
+    //         $("#status").html("<p>You won!!</p>");
+    //     };
+    //     else{
+    //         $("#status").html("<p>You lost!</p>");
+    //     };        
+    // };
     // else if (accumulated> goalGenerator()){
     //     $("#loss").html(loss++);
     // };
